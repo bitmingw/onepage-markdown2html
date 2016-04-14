22a39766d10a93a9244690d8a9d94702bdd3c0b4
Ming Wen <bitmingw@gmail.com>
2016-04-14 17:36:52 -0400

    feat(generator): add html template and generator

---
+++ genhtml.js
@@ -0,0 +1,41 @@
+var marked = require('marked');
+var fs = require('fs');
+
+marked.setOptions({
+	highlight: function (code) {
+		return require('highlight.js').highlightAuto(code).value;
+	}
+});
+
+var headfilename = 'header.html';
+var stylefilename = 'style-script.html'
+var footfilename = 'footer.html';
+
+// specify you own file here
+var infilename = 'test.md';
+var outfilename = 'test.html';
+
+fs.readFile(headfilename, {encoding: 'utf8'}, function(err, data) {
+	if (err) throw err;
+	fs.writeFile(outfilename, data, function () {
+	});
+});
+
+fs.readFile(stylefilename, {encoding: 'utf8'}, function(err, data) {
+	if (err) throw err;
+	fs.appendFile(outfilename, data, function () {
+	});
+});
+
+fs.readFile(infilename, {encoding: 'utf8'}, function(err, data) {
+	if (err) throw err;
+	fs.appendFile(outfilename, marked(data), function() {
+	});
+});
+
+fs.readFile(footfilename, {encoding: 'utf8'}, function(err, data) {
+	if (err) throw err;
+	fs.appendFile(outfilename, data, function () {
+		console.log('Generate HTML successfully!');
+	});
+});
